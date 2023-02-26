d559467cf259c68436a99e73414f5e12d71c7245
Cryptogoldenergy <cfrylinck1997@gmail.com>
2023-02-26 20:16:43 +0200

    Updated content

--- src/Components/Pages/WhyUseUs/whyUseUsPage.js
+++ src/Components/Pages/WhyUseUs/whyUseUsPage.js
@@ -6,7 +6,7 @@ const WhyUseUsPage = () => {
         <div className='whyUseUs-page'>
             <div className='whyUseUs-container'>
                 <h3 id="top">Why choose Kroonwater </h3>
-                <p className='description-heading'>Our main aim is to establish lasting relationships with our clients, by providing them with: </p>
+                <p className='description-heading bold'>Our main aim is to establish lasting relationships with our clients, by providing them with: </p>
                 <ul>
                     <li>Top quality water with oxygen enriched, for personal and commercial use </li>
                     <li>Excellent quality products for household and industrial use </li>
@@ -19,7 +19,7 @@ const WhyUseUsPage = () => {
                 The activated carbon used in the different filters is coconut shell carbon. Coconut shell carbon is a much more effective and more expensive carbon than coal-based carbon, but most importantly is safe to use. 
                 <p>Coal-based carbon is prone to leach dangerous heavy metals into the water, which is especially dangerous to use in the taste and odour filter. </p>
                 <p>Coal-based carbon is also not effective in the removal of endocrine disrupting hormones. </p>
-                <p className='description-heading'>The biggest advantages of the Hydro Wellness RO is the pre-filter size: </p>
+                <p className='description-heading bold'>The biggest advantages of the Hydro Wellness RO is the pre-filter size: </p>
                 <ul>
                     <li>The 15% bigger filter provides the client with an extended lifetime on the filters as well as better protection to the membrane </li>
                     <li>The plastic plate at the bottom of the 5 micron filter ensures that the filters seal more securely and reduces the risk of water bypassing the filter. </li>
@@ -27,12 +27,12 @@ const WhyUseUsPage = () => {
                 <h3>Our Products</h3>
                 <p>Hydro Wellness 5 Stage Reverse Osmosis Purification System. </p>
                 <p>Removes microscopic debris and bacteria without removing healthy minerals from water. </p>
-                <p className='description-heading'>Features & Benefits: </p>
+                <p className='description-heading bold'>Features & Benefits: </p>
                 <ul>
                     <li>Low maintenance and longer life for filters </li>
                     <li>Reduces risk of water bypassing the filters </li>
                 </ul>
-                <p className='description-heading more-on-top'>What you get: </p>
+                <p className='description-heading more-on-top bold'>What you get: </p>
                 <ul>
                     <li>5 Micron Sediment Filter</li>
                     <li>Carbon Filter </li>
@@ -42,6 +42,29 @@ const WhyUseUsPage = () => {
                     <li>Faucet</li>
                     <li>Pipe and Bracket</li>
                 </ul>
+                <h3>Water Softener System</h3>
+                <p>Reduces lime and hardening elements in the water</p>
+                <h3>What you get: </h3>
+                <p>Reduces lime and hardening elements in the water </p>
+                <p className='description-heading bold'>Features & Benefits: </p>
+                <ul>
+                    <li>Cost-effective</li>
+                    <li>Removes lime scale from water</li>
+                    <li>Softer water protects elements in kettles, geysers and pipes </li>
+                    <li>Soft water ensures cleaner laundry, dishes and hair </li>
+                </ul>
+                <h3>Tips & Tricks </h3>
+                <p className='description-heading bold'>How long does an industrial water filter last?</p>
+                <p>Filters used in commercial settings should be changed every 4 to 6 months. </p>
+                <p>Filters in residential settings should be changed every 6 to 12 months. </p>
+                <p>Reverse osmosis, membranes and additional alkalisers only need to be changed every 2 or 4 years, respectively. </p>
+                
+                <h3>Facts to consider</h3>
+                <p>Regular maintenance can extend the life of your water purification equipment. </p>
+                <p>Water softening can improve the efficiency of your appliances and reduce the amount of soap and detergent you use. </p>
+                <p>Hard water can leave mineral build-up on your fixtures and appliances.  Kroonwater offers water softening products to prevent this damage. </p>
+                <p>Coal-based carbon is prone to leach dangerous heavy metals into the water, which is especially dangerous to use in the taste and odour filter.  </p>
+                <p>The activated carbon used in the different filters is coconut shell carbon. Coconut shell carbon is a much more effective and more expensive carbon than coal-based carbon, but most importantly is safe to use. </p>
             </div>
         </div>
     )
