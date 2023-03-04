237d172d0295fffb399227d61eadefda0af6cc8a
Cryptogoldenergy <cfrylinck1997@gmail.com>
2023-03-04 20:49:14 +0200

    Updated sitemap routes

--- src/sitemap-routes.js
+++ src/sitemap-routes.js
@@ -3,12 +3,11 @@ import { Route } from 'react-router';
  
 export default (
     <Route>
-    <Route path='/contact' ></Route>
-    <Route path='/disclaimer' ></Route>
-    <Route path='/products/:categoryId' ></Route>
-    <Route path='/product/:productId' ></Route>
-    <Route path='/services' ></Route>
-    <Route path='/whyUseUs' ></Route>
-    <Route path='/' ></Route>
+        <Route path='/contact' ></Route>
+        <Route path='/disclaimer' ></Route>
+        <Route path='/products/:categoryId' ></Route>
+        <Route path='/product/:productId' ></Route>
+        <Route path='/services' ></Route>
+        <Route path='/whyUseUs' ></Route>
     </Route>
 );
