0fd931669092d5379b8e90b7e8941eb00d04c4d0
Cryptogoldenergy <cfrylinck1997@gmail.com>
2023-03-04 20:45:12 +0200

    Added sitemap generation

---
+++ src/sitemap-generator.js
@@ -0,0 +1,35 @@
+require("babel-register")({
+    presets: ["es2015", "react"]
+  });
+   
+  const router = require("./sitemap-routes").default;
+  const Sitemap = require("react-router-sitemap").default;
+  
+  function generateSitemap() {
+    let prodIdMap = [];
+    let catIdMap = [];
+
+    for(var i = 1; i <= 17; i++) {
+        prodIdMap.push({ id: i });
+    }
+
+    catIdMap.push({id: "undefined"});
+
+    for(var i = 1; i <= 3; i++) {
+        catIdMap.push({ id: i });
+    }
+
+    const paramsConfig = {
+      "/product/:productId": prodIdMap,
+      "/products/:categoryId": catIdMap
+    };
+
+    return (
+    new Sitemap(router)
+        .applyParams(paramsConfig)
+        .build("https://kroonwater.com")
+        .save("./public/sitemap.xml")
+    );
+  }
+  
+  generateSitemap();
---
+++ src/sitemap-routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router';
+ 
+export default (
+    <Route>
+    <Route path='/contact' ></Route>
+    <Route path='/disclaimer' ></Route>
+    <Route path='/products/:categoryId' ></Route>
+    <Route path='/product/:productId' ></Route>
+    <Route path='/services' ></Route>
+    <Route path='/whyUseUs' ></Route>
+    <Route path='/' ></Route>
+    </Route>
+);
